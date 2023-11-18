f3794b8870a4ebcb4c6345660fcbe7f248fb7e4e
FarhalBasu <basu.farhal@gmail.com>
2023-11-18 19:47:59 +0530

    first commit

---
+++ src/utils/api.js
@@ -0,0 +1,16 @@
+import axios from "axios";
+
+const BASE_URL = "https://youtube138.p.rapidapi.com";
+
+const options = {
+    params: { hl: "en", gl: "US" },
+    headers: {
+        'X-RapidAPI-Key': '43e887ab02mshbf5f9761538b33fp11d59djsn3e073419f391',
+        "X-RapidAPI-Host": "youtube138.p.rapidapi.com",
+    },
+};
+
+export const fetchDataFromApi = async (url) => {
+    const { data } = await axios.get(`${BASE_URL}/${url}`, options);
+    return data;
+};
